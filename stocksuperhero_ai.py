3178f364c1dd58c0524d2eae482a182492988796
kittycattoys <ninedynamics@gmail.com>
2024-11-16 14:55:59 -0700

    T

--- stocksuperhero_ai.py
+++ stocksuperhero_ai.py
@@ -5,6 +5,15 @@ from supabase import create_client, Client
 from datetime import datetime
 import time
 from functions.vector_search import get_supabase_dataframe
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import sentencepiece
+
+print(sentencepiece.__version__)
+model_id = "Jeffsimpsons/Llama-3.2-1B-Instruct-Q4_K_M-GGUF"
+filename = "llama-3.2-1b-instruct-q4_k_m.gguf"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id, gguf_file=filename)
+model = AutoModelForCausalLM.from_pretrained(model_id, gguf_file=filename)
 
 # Set page configuration as the first Streamlit command
 st.set_page_config(layout="wide")
@@ -27,28 +36,30 @@ input_v_rsi = df_dim['v_rsi'][0]   # Example embedding vector for v_rsi
 df_vector_search = get_supabase_dataframe(input_v_ps, input_v_rsi, match_count=10)
 st.write("Vector Search Results")
 st.dataframe(df_vector_search)
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 with st.expander("Expander with scrolling content", expanded=True):
    with st.container(height=300):
-       
-        # Initialize chat history
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
-    
         # Display chat messages from history on app rerun
         for message in st.session_state.messages:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
     
-        if prompt := st.chat_input("What is up?"):
-            # Display user message in chat message container
-            with st.chat_message("user"):
-                st.markdown(prompt)
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            response = f"Echo: {prompt}"
-            # Display assistant response in chat message container
-            with st.chat_message("assistant"):
-                st.markdown(response)
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response})
+if prompt := st.chat_input("Ask Stock Superhero AI"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# Ask a question
+question = "What is the capital of France?"
+inputs = tokenizer(question, return_tensors="pt")
+
+# Generate and decode the response
+outputs = model.generate(**inputs, max_length=50)
+response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+print("Response:", response)
